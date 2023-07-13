9c0435e1410248d0975a0036ef60a2f09718e5a9
FacundoSalazar <facundev@outlook.com>
2023-07-13 10:21:47 -0300

    Uploading portfolio to GitHub and including Tailwind.

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primaryColor: '#333333',
+				secondaryColor: '#f2f2f2',
+				tertiaryColor: 'rgb(255, 192, 83)',
+				fourthColor: 'rgb(225, 225, 225)',
+			},
+			backgroundColor: {
+				secondaryColor: '#f2f2f2'
+			}
+		},
+	},
+	plugins: [],
+}
